25ad2a0999a3b24c3dd15e974f75e6b996374fdd
Oleg <upliftingpark@gmail.com>
2024-10-05 14:24:10 +0300

    feat: add ios launch support

--- echo/lib/main.dart
+++ echo/lib/main.dart
@@ -1,30 +1,51 @@
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:echo/features/login_page/login_page.dart';
 import 'package:echo/services/auth_service.dart';
 import 'package:echo/services/storage.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'features/main_page/main_page.dart';
 import 'features/register_page/register_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runZonedGuarded(() {
+    runApp(const MyApp());
+  }, (object, stackTrace) {
+    log(object.toString(), stackTrace: stackTrace);
+  });
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late final Future<String?> userFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    userFuture = checkToken();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: FutureBuilder<String?>(
-        future: checkToken(),
+        future: userFuture,
         builder: (BuildContext context, AsyncSnapshot<String?> snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
             String? user = snapshot.data;
             return Navigator(
               initialRoute:
-              user == null ? LoginPage.routeName : MainPage.routeName,
+                  user == null ? LoginPage.routeName : MainPage.routeName,
               onGenerateRoute: (RouteSettings settings) {
                 if (settings.name == MainPage.routeName) {
                   return MaterialPageRoute(

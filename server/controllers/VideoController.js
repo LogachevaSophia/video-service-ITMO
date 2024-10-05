784fca75f4bf38d77a3a0a08d5a16fe3ff827586
Sophia <lsofa1204@gmail.com>
2024-10-05 17:39:27 +0300

    SCRUM-49 изменение схемы

--- server/controllers/VideoController.js
+++ server/controllers/VideoController.js
@@ -2,11 +2,11 @@ const db = require('../db/connection');
 const logger = require('../logger/logger');
 
 exports.upload = async (req, res) => {
-    const { Link, Name } = req.body;
+    const { Link, Name, Preview } = req.body;
     const userId = req.user.id;
     logger.info('Attemting to upload video by link');
     try {
-        const [video] = await db.query('INSERT INTO video (Name, Link, Cost) VALUES (?, ?, ?)', [Name, Link, 100])
+        const [video] = await db.query('INSERT INTO video (Name, Link, Cost, Author, Preview) VALUES (?, ?, ?, ?, ?)', [Name, Link, 100, userId, Preview])
         res.status(201).json({ message: "Video uploaded", videoId: video.insertId })
     }
     catch (e) {
@@ -24,7 +24,7 @@ exports.delete = async  (req, res) => {
         res.status(201).json({ message: "Video deleted" })
     }
     catch (e) {
-        logger.error(`Failed to upload video Link=${Link} at user=${userId}, error=${e.message}`)
+        logger.error(`Failed to delete video id=${Id} at user=${userId}, error=${e.message}`)
         res.status(500).json({ error: `${e.message}` })
     }
 }
@@ -32,8 +32,7 @@ exports.delete = async  (req, res) => {
 exports.getAllItems = async (req, res) => {
     logger.info('Attemting to get all videos');
     try{
-
-        const result = await db.query('Select Name, Link from video');
+        const result = await db.query('Select Id, video.Name, Link, Preview, PersonId, user.Name as UserName, Email from video left join User on User.personId=video.Author');
         console.log(result);
         logger.info('successfully load all videos');
         res.status(200).json({data: result[0]})
